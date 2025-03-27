b3e631368fcdcecdc22a8171b0f245895bd070c8
Juan Lowy <juanmontenegro58@gmail.com>
2025-03-27 08:40:15 -0500

    Configuración despliegue

---
+++ fup/settings/production.py
@@ -0,0 +1,37 @@
+from .base import *
+
+import os
+
+from .base import *
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-@==x%vz4gjnv2&4pn@@*_r&o2o4c&0+r*l0+#0kdjflm@!ybc2c'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8080",
+]
+
+ALLOWED_HOSTS = [
+    '*',
+    'fup-env.eba-ijpp3mdu.us-east-1.elasticbeanstalk.com'
+]
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fup',
+        'USER': 'fup_admin',
+        'PASSWORD': 'fup-database.c0l2au4w2p33.us-east-1.rds.amazonaws.com',
+        'HOST': 'db',
+        'PORT': 3306
+    }
+}
