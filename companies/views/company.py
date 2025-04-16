b9089f9895233f43d98c4706d39272396bc7a499
Juan Lowy <juanmontenegro58@gmail.com>
2025-04-15 20:18:18 -0500

    Se agregan filtros para empresas y programas

--- companies/views/company.py
+++ companies/views/company.py
@@ -1,4 +1,8 @@
-from rest_framework import viewsets
+from rest_framework import (
+    viewsets,
+    filters
+)
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_spectacular.utils import (
     extend_schema,
     extend_schema_view
@@ -43,8 +47,8 @@ class CompanyViewSet(viewsets.ModelViewSet):
     serializer_class = CompanyModelSerializer
     queryset = Company.objects.all()
     http_method_names = ['get', 'post', 'put']
-    filterset_class = CompanyFilter
-    ordering_fields = ['name']
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ['name', 'nui']
 
     def get_serializer_class(self):
         if self.action == 'retrieve':
